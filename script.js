2666fc59654e7374c0a178bd83231b25a1d71647
Nikhil <nksingh7000@gmail.com>
2025-12-28 06:33:21 +0530

    unkidnap feature

---
+++ script.js
@@ -0,0 +1,9 @@
+function kidnap() {
+    if(document.getElementById('myImage').style.display=='none'){
+        document.getElementById('myImage').style.display='block';
+    }
+    else{
+        document.getElementById('myImage').style.display='none';
+    }
+    ;
+}
